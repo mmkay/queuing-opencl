9a7714d252af20c3ba9bc04fc4802c31acdb82cd
Mateusz Kulewicz <matikula9@gmail.com>
2014-05-20 21:18:06 +0200

    Kernel added to java file (OpenCL code doesn't compile yes)

--- queuing/src/main/java/pl/edu/pg/queuing/App.java
+++ queuing/src/main/java/pl/edu/pg/queuing/App.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.nio.FloatBuffer;
 import java.util.Random;
 
-import com.jogamp.opencl.CLBuffer;
 import com.jogamp.opencl.CLCommandQueue;
 import com.jogamp.opencl.CLContext;
 import com.jogamp.opencl.CLDevice;
@@ -37,45 +36,29 @@ public class App
             // create command queue on device.
             CLCommandQueue queue = device.createCommandQueue();
 
-            int elementCount = 1444477; // Length of arrays to process
+            int elementCount = 1; // Length of arrays to process
             int localWorkSize = Math.min(device.getMaxWorkGroupSize(), 256); // Local work size dimensions
             int globalWorkSize = roundUp(localWorkSize, elementCount); // rounded up to the nearest multiple of the localWorkSize
 
             // load sources, create and build program
-            CLProgram program = context.createProgram(App.class.getResourceAsStream("VectorAdd.cl")).build();
-
-            // A, B are input buffers, C is for the result
-            CLBuffer<FloatBuffer> clBufferA = context.createFloatBuffer(globalWorkSize, READ_ONLY);
-            CLBuffer<FloatBuffer> clBufferB = context.createFloatBuffer(globalWorkSize, READ_ONLY);
-            CLBuffer<FloatBuffer> clBufferC = context.createFloatBuffer(globalWorkSize, WRITE_ONLY);
-
-            System.out.println("used device memory: "
-                + (clBufferA.getCLSize()+clBufferB.getCLSize()+clBufferC.getCLSize())/1000000 +"MB");
-
-            // fill input buffers with random numbers
-            // (just to have test data; seed is fixed -> results will not change between runs).
-            fillBuffer(clBufferA.getBuffer(), 12345);
-            fillBuffer(clBufferB.getBuffer(), 67890);
+            CLProgram program = context.createProgram(App.class.getResourceAsStream("kernel.cl")).build();
+            
+            float rejected = 0;
 
             // get a reference to the kernel function with the name 'VectorAdd'
             // and map the buffers to its input parameters.
-            CLKernel kernel = program.createCLKernel("VectorAdd");
-            kernel.putArgs(clBufferA, clBufferB, clBufferC).putArg(elementCount);
+            CLKernel kernel = program.createCLKernel("RunSimulation");
+            kernel.putArg(rejected);
 
             // asynchronous write of data to GPU device,
             // followed by blocking read to get the computed results back.
             long time = nanoTime();
-            queue.putWriteBuffer(clBufferA, false)
-                 .putWriteBuffer(clBufferB, false)
-                 .put1DRangeKernel(kernel, 0, globalWorkSize, localWorkSize)
-                 .putReadBuffer(clBufferC, true);
+            queue.put1DRangeKernel(kernel, 0, globalWorkSize, localWorkSize);
             time = nanoTime() - time;
 
             // print first few elements of the resulting buffer to the console.
-            out.println("a+b=c results snapshot: ");
-            for(int i = 0; i < 10; i++)
-                out.print(clBufferC.getBuffer().get() + ", ");
-            out.println("...; " + clBufferC.getBuffer().remaining() + " more");
+            
+            out.println("Rejected elements: " + rejected);
 
             out.println("computation took: "+(time/1000000)+"ms");
             
